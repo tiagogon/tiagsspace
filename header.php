d5f8ca8f307f0b74de00cd0b9ec4acedb5658d84
Tiago <tiago@tiagogoncalves.com>
2018-04-20 19:57:41 +0200

    redeuce font load

--- header.php
+++ header.php
@@ -44,7 +44,7 @@
 		<?php // ----- FONTS ----- ?>
 			<!-- <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/library/fonts/font-awesome/css/font-awesome.min.css"> -->
 			<!-- <link href='https://fonts.googleapis.com/css?family=Raleway:600,600italic,800,800italic' rel='stylesheet' type='text/css'> -->
-			<link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+			<link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,400i,600,600i" rel="stylesheet">
 
 			<!-- <link href="https://fonts.googleapis.com/css?family=Work+Sans:600,800" rel="stylesheet">
 			<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,700i,800,800i" rel="stylesheet">
