b1a3daa0465fe7babdd29be6cf4928b557f4a78d
Tiago <tiago@tiagogoncalves.com>
2018-11-27 21:43:42 +0100

    polish mobile

--- template-parts/front-social.php
+++ template-parts/front-social.php
@@ -4,25 +4,22 @@
 			<div class="<?php content_wrap() ?>">
 				<ul class="social-links">
 					<li>
-						<a href="https://www.instagram.com/tiagsssss/" data-hover="Facebook" target="_blank">Insta</a>
-					</li>
-					<li>
-						<a href="https://soundcloud.com/tiagsssss" data-hover="SoundCloud" target="_blank">Soundcloud</a>
-					</li>
-					<li>
-						<a href="https://twitter.com/tiagsssss" data-hover="Twitter" target="_blank">Twitter</a>
-					</li>
-					<li>
-						<a href="https://vimeo.com/tiags" data-hover="Tumbler" target="_blank">Vimeo</a>
+						<a href="https://www.instagram.com/tiagsssss/" data-hover="Facebook" target="_blank">Instagram</a>
 					</li>
 					<li>
 						<a href="https://troubleplace.tumblr.com" data-hover="SoundCloud" target="_blank">Tumblr</a>
 					</li>
 					<li>
-						<a href="https://imtakingtwo.tumblr.com" data-hover="SoundCloud" target="_blank"> References</a>
+						<a href="https://soundcloud.com/tiagsssss" data-hover="SoundCloud" target="_blank">Soundcloud</a>
 					</li>
 					<li>
-						<a href="https://trouble.place/feed" data-hover="Feed" target="_blank">Feed</a>
+						<a href="https://vimeo.com/tiags" data-hover="Tumbler" target="_blank">Vimeo</a>
+					</li>
+					<li>
+						<a href="https://twitter.com/tiagsssss" data-hover="Twitter" target="_blank">Twitter</a>
+					</li>
+					<li>
+						<a href="https://imtakingtwo.tumblr.com" data-hover="SoundCloud" target="_blank"> References</a>
 					</li>
 				</ul>
 				<div class="email-form">
