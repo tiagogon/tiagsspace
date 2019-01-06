e94402d953c79dad9a2b1a13b38ab8a491d17d34
Tiago <tiago@tiagogoncalves.com>
2019-01-06 18:46:22 +0100

    change social media links

--- template-parts/front-social.php
+++ template-parts/front-social.php
@@ -7,7 +7,7 @@
 						<a href="https://www.instagram.com/tiagsssss/" data-hover="Facebook" target="_blank">Instagram</a>
 					</li>
 					<li>
-						<a href="https://troubleplace.tumblr.com" data-hover="SoundCloud" target="_blank">Tumblr</a>
+						<a href="https://tiagsssss.tumblr.com/" data-hover="SoundCloud" target="_blank">Tumblr</a>
 					</li>
 					<li>
 						<a href="https://soundcloud.com/tiagsssss" data-hover="SoundCloud" target="_blank">Soundcloud</a>
@@ -19,11 +19,11 @@
 						<a href="https://twitter.com/tiagsssss" data-hover="Twitter" target="_blank">Twitter</a>
 					</li>
 					<li>
-						<a href="https://imtakingtwo.tumblr.com" data-hover="SoundCloud" target="_blank"> References</a>
+						<a href="https://tiags.tumblr.com/" data-hover="SoundCloud" target="_blank"> References</a>
 					</li>
 				</ul>
 				<div class="email-form">
-					<form action="https://tinyletter.com/trouble-letter" method="post" target="popupwindow" onsubmit="window.open('https://tinyletter.com/trouble-letter', 'popupwindow', 'scrollbars=yes,width=800,height=600');return true">
+					<form action="https://tinyletter.com/tiagsssss" method="post" target="popupwindow" onsubmit="window.open('https://tinyletter.com/tiagsssss', 'popupwindow', 'scrollbars=yes,width=800,height=600');return true">
 						<input class="input-field" type="text" name="email" id="tlemail" placeholder="Your email address"/>
 						<input type="hidden" value="1" name="embed"/>
 						<input class="input-button" type="submit" value="Subscribe" />
