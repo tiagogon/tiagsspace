65064b1d36da9f55fb9c87517cb8ce4a9fab2aa8
Tiago <tiago@tiagogoncalves.com>
2019-07-15 21:12:05 +0200

    fix bug where video as last post broke the grid

--- template-parts/single-gallery.php
+++ template-parts/single-gallery.php
@@ -576,7 +576,9 @@ if (!get_field('deactivate_gallery')) {
                         // add item-sizer for Masonry responsive calculations
                         if ($count_item == 1 AND $number_of_columns_lg > 1 AND $deactivat_masonry == false) { ?>
 
-                            <div class="item-sizer <?php echo $class_thumbnail_without_factor;?>"></div>
+                            <!-- <div class="item-sizer <?php echo $class_thumbnail_without_factor;?>"></div> -->
+                            <div class="item-sizer <?php echo $class_thumbnail;?>
+
                         <?php }?>
 
 
