a90a830e944116aa5edbde272d10a202a63ce238
Tiago <tiago@tiagogoncalves.com>
2021-02-05 22:03:20 +0100

    fix copy

--- header.php
+++ header.php
@@ -203,7 +203,7 @@
 				<div class="submenu submenu-log col-xs-48 col-sm-48 col-md-48">
 					<ul>
 
-						<li><a href="<?php echo get_post_type_archive_link( 'log'); ?>" class="<?php if (is_post_type_archive('log')) { echo "active";} ?>">Space</a></li>
+						<li><a href="<?php echo get_post_type_archive_link( 'log'); ?>" class="<?php if (is_post_type_archive('log')) { echo "active";} ?>">All</a></li>
 
 						<li><a href="<?php echo get_term_link( 'blwww', 'log-branch'); ?>" class="<?php if (is_tax('log-branch','blwww')) { echo "active";} if ((is_single() and has_term( 'blwww', 'log-branch' ))) { echo " belongs";} ?>">blwww</a></li>
 
