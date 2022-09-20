6be76d28cb970c6d12b43934225a3e578c9e2ce0
Tiago Gonçalves <tiago@tiagogoncalves.com>
2022-09-20 21:43:23 +0200

    f

--- template-parts/single-content.php
+++ template-parts/single-content.php
@@ -99,12 +99,12 @@ if (is_singular() && !is_page()) {
             <p class= "footer-meta">
                 <?php echo taxonomy_list($post->ID,'places', '', ', ', ', ', ' & ', 'link'); ?><?php echo taxonomy_list($post->ID,'from', '', '. ', ', ', ' & ', 'link'); ?>
                 <?php if ($obj->labels->name == "Posts") {
-                    echo "";
+                    echo "Published ";
                 } else {
-                    echo '<a href="'.get_post_type_archive_link( $post_type ).'">'.$obj->labels->name.'</a>'.$logs_branch.', ';
+                    echo 'Published under <a href="'.get_post_type_archive_link( $post_type ).'">'.$obj->labels->name.'</a>'.$logs_branch.', ';
                 } ?>
 
-                <time itemprop="datePublished" datetime="<?php the_time( 'c' ); ?>" content="<?php the_time( 'c' ); ?>"><?php the_time('M j, Y'); //the_time('d/m/Y'); ?></time>. 
+                on <time itemprop="datePublished" datetime="<?php the_time( 'c' ); ?>" content="<?php the_time( 'c' ); ?>"><?php the_time('M j, Y'); //the_time('d/m/Y'); ?></time>.
 
                 <?php echo $taxonomies_string.' ';
                 // Edit link
