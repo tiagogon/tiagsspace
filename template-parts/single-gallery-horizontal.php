857e080dd2aa6f4bb2a36168d05c724bf6c6a574
Tiago Gonçalves <tiago@tiagogoncalves.com>
2023-06-08 21:45:25 +0200

    faster

--- template-parts/single-gallery-horizontal.php
+++ template-parts/single-gallery-horizontal.php
@@ -357,7 +357,7 @@ if (!get_field('deactivate_gallery')) {
                     if (get_field('still_duration')) {
                         echo get_field('still_duration')+rand(-1000, 1000);
                    } else {
-                     echo 2000+rand(-300, 300);
+                     echo 2000+rand(-200, 200);
                    }
                 ?>, // defaul is 3000ms = 4 heart beats
                disableOnInteraction: false,
