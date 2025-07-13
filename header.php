6e7f187cd40f11893c8e6a2cdfc110898c75d031
Tiago Gonçalves <tiago@tiagogoncalves.com>
2025-07-14 00:26:33 +0200

    update test deployment comment in header.php to v4

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
 
 <html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
 
-			<?php // ----- test deployment v3 ----- ?>
+			<?php // ----- test deployment v4 ----- ?>
 
 	<head>
 		<?php // ----- META ----- ?>
