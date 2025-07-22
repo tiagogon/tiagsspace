11b3cbc240f84a123173bb72877f3c39260c6054
Tiago Gonçalves <tiago@tiagogoncalves.com>
2025-07-22 23:37:42 +0200

    back

--- template-parts/single-video_selfhosted.php
+++ template-parts/single-video_selfhosted.php
@@ -123,9 +123,9 @@ $json = '{
                     <?php endforeach; ?>
                     Your browser does not support the video tag.
                     <!-- Caption files -->
-                    <!-- <track kind="captions" label="English" srclang="en" src=""
+                    <track kind="captions" label="English" srclang="en" src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.en.vtt"
                             default>
-                    <track kind="captions" label="Français" srclang="fr" src=""> -->
+                    <track kind="captions" label="Français" srclang="fr" src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.fr.vtt">
                     <!-- Fallback for browsers that don't support the <video> element -->
                     <a href="<?php echo esc_url($original_url); ?>" download>Download</a>
                 </video>
