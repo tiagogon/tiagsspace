d763dee3f456943bb877bb1e434b4299c4fa21fe
Tiago Gonçalves <tiago@tiagogoncalves.com>
2026-03-13 17:32:38 +0100

    Update email link text from 'Email' to 'Mail' in header.php

--- header.php
+++ header.php
@@ -299,7 +299,7 @@
 				<div class="<?php echo $menu_groups_class; ?>" >
 					<ul>
 					<li>
-						<a href="mailto:mail@tiags.space" target="_blank">Email</a>
+						<a href="mailto:mail@tiags.space" target="_blank">Mail</a>
 					</li>
 					<li>
 						<a href="https://www.instagram.com/tiagsssss/" target="_blank">Instagram</a>
